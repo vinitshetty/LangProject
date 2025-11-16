69469dd8356c0aafaf50a89761eeb077e6d66b30
Vinutkumar Shetty <vinitshetty1989@gmail.com>
2025-11-16 20:55:17 +0800

    langchain + langwatch integration

--- chat.py
+++ chat.py
@@ -1,5 +1,12 @@
 import chainlit as cl
-import mistral as ms
+#import mistral as ms
+import langchain as lc
+import os
+import langwatch
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 @cl.on_chat_start
 async def start():
@@ -13,6 +20,6 @@ async def handle_message(message: cl.Message):
     msg = cl.Message(content="")  # empty placeholder message
     await msg.send()
 
-    for token in ms.call_mistral_api(user_input):
+    for token in lc.call_mistral_api(user_input):
         msg.content += token
         await msg.update()  # updates the already-sent message
