1f71ea89a2c4f35991d0c87e3d821d4eff5b0952
Vinutkumar Shetty <vinitshetty1989@gmail.com>
2025-11-23 10:42:08 +0800

    clean

--- mistral.py
+++ mistral.py
@@ -37,20 +37,4 @@ def call_mistral_api(user_input='What is the best French cheese and explain to m
     )
     return chat_response.choices[0].message.content
 
-'''
-for chunk in client.chat.stream(
-    model=model,
-    messages=[
-        {"role": "system", "content": "Always give best answer in completeness around 25 words."},
-        {"role": "user", "content": "What is the best French cheese and explain to me?"},
-    ],
-    max_tokens=50,
-    temperature=0.9,
-    top_p=0.9,
-    n=1,
-    timeout_ms=30000,
-):
-    print(chunk.data.choices[0].delta.content, end="", flush=True)
-'''
-
 print(call_mistral_api())
